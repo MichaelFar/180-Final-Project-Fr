6d20ba0ed5e6769f09ee7169f07ac408eea83465
FDSGASFSAEFS <cwallingtpn16@gmail.com>
2024-12-09 12:14:06 -0800

    ..

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -32,6 +32,6 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         //scoreText.text = "Score: " + Player.Score;
-       // livesText.text = "Lives: " + WaveSingleton.playerDamageable.heal
+        livesText.text = "Lives: " + WaveSingleton.playerDamageable.health;
     }
 }
